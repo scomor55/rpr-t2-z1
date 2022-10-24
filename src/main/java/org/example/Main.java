a185666d8b6941fb929abf8c7f6c87be2f7d54df
Safet Čomor <scomor2@etf.unsa.ba>
2022-10-24 14:47:37 +0200

    rpr-t2-z1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,22 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+
+    /*public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int broj = in.nextInt();
+        System.out.println(Matematika.sin(broj));
+
+    }*/
+
+    public static void Main(String[] args){
+
+        if(args[0].equals("sin")) {
+            System.out.println(Matematika.sin(Double.parseDouble(args[1])));
+        }
+        }
     }
+
 }
--- src/main/java/org/example/Matematika.java
+++ src/main/java/org/example/Matematika.java
@@ -1,2 +1,28 @@
-package org.example;public class Matematika {
+package org.example;
+
+public class Matematika {
+
+
+    public static int faktorijel(int broj){
+        int f = 1;
+        for(int i = 1; i < broj ; i++) {
+            f += f * i;
+        }
+        return f;
+    }
+
+    public static double sin(double broj){
+        int predznak = 1,eksponent = 1;
+        double suma = 0;
+        broj = broj  * Math.PI/180;
+        for(int i = 1; i <= 10 ; i++){
+            suma += predznak *(Math.pow(broj,eksponent)/faktorijel(eksponent) );
+            predznak *= -1;
+            eksponent += 2;
+        }
+        return suma;
+    }
 }
+
+
+
